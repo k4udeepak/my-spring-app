40399de5e476d8f5541214939faf6424cb6bffdf
k4udeepak <p4udeepak@gmail.com>
2025-07-09 14:16:22 +0530

    commit for automated STS - GIT

--- src/main/java/com/example/myspringboot/MyController.java
+++ src/main/java/com/example/myspringboot/MyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 	@GetMapping("/hello")
 	public void showMessage() {
-		System.out.println("Here i am, your first Spring project");
+		System.out.println("Here i am, your first Spring project automated");
 	}
 
-}
+}   
