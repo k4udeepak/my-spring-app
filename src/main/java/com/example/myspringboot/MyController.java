d310e55bdb833366557ae42045eb1e2a99f94a70
k4udeepak <p4udeepak@gmail.com>
2025-07-09 14:04:02 +0530

    Initial commit

---
+++ src/main/java/com/example/myspringboot/MyController.java
@@ -0,0 +1,13 @@
+package com.example.myspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	@GetMapping("/hello")
+	public void showMessage() {
+		System.out.println("Here i am, your first Spring project");
+	}
+
+}
